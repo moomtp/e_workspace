22ab2cd4ebd211f3cfb8d1f7ed2435de316cff6e
moomtp <85488108+moomtp@users.noreply.github.com>
2024-10-15 07:43:02 +0800

    Create linux_install_script.sh

---
+++ linux_install_script.sh
@@ -0,0 +1,11 @@
+sudo apt install git
+sudo apt install python3
+
+
+sudo apt install nvidia-driver-<版本>   ＃ for nv
+sudo apt install mesa-vulkan-drivers\   ＃ for amd
+
+＃ for proton 
+sudo apt install vulkan-tools
+sudo apt upgrade mesa*
+
