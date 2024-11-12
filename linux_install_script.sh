00464109568ce96436783647ac2a39e1ba97a78d
moomtp <85488108+moomtp@users.noreply.github.com>
2024-11-12 15:52:45 +0800

    Update linux_install_script.sh

--- linux_install_script.sh
+++ linux_install_script.sh
@@ -2,6 +2,8 @@ sudo apt update
 sudo apt upgrade
 sudo apt install snapd
 
+
+
 sudo apt install git
 sudo apt install python3
 
@@ -16,6 +18,9 @@ sudo add-apt-repository multiverse
 sudo apt install steam
 
 
+
+sudo apt install ibus ibus-chewing
+ibus-setup
 
 sudo apt install nvidia-driver-<版本>   ＃ for nv
 sudo apt install mesa-vulkan-drivers\   ＃ for amd
