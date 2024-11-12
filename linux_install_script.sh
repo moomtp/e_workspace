66e3f604df8880f596829fd19ca8b525a32c7f96
moomtp <85488108+moomtp@users.noreply.github.com>
2024-11-12 15:56:41 +0800

    Update linux_install_script.sh

--- linux_install_script.sh
+++ linux_install_script.sh
@@ -11,16 +11,19 @@ sudo snap install chromium
 
 sudo snap install code --classic
 
-sudo snap install discord
+sudo snap install discord # 安裝dis
 
-
+# 安裝steam
 sudo add-apt-repository multiverse
 sudo apt install steam
 
 
-
+＃ 安裝注音
 sudo apt install ibus ibus-chewing
 ibus-setup
+
+＃ 升級ubuntu
+sudo do-release-upgrade -d
 
 sudo apt install nvidia-driver-<版本>   ＃ for nv
 sudo apt install mesa-vulkan-drivers\   ＃ for amd
