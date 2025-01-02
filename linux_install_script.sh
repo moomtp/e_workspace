cfa91f8fa9f4e712d747968a85ec12be575f686d
moomtp <moomtp@gmail.com>
2025-01-02 17:51:28 +0800

    vault backup: 2025-01-02 17:51:28

--- linux_install_script.sh
+++ linux_install_script.sh
@@ -6,6 +6,7 @@ apt install vim
 sudo snap install nvim --classic
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
+# 到nerd font 下載字體 ： https://www.nerdfonts.com/font-downloads
 
 apt install zsh -y
 chsh -s $(zsh)
@@ -18,26 +19,26 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # 更改zshrc文件
 vim ~/.zshrc
-```.zshrc
-# 修改主题
-ZSH_THEME="powerlevel10k/powerlevel10k"
+$()$(
+  .zshrc
+  # 修改主题
+  ZSH_THEME="powerlevel10k/powerlevel10k"
 
-# 启用插件
-plugins=(
-  git
-  zsh-autosuggestions
-  zsh-syntax-highlighting
-)
-```
+  # 启用插件
+  plugins=(
+    git
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+  )
+)$()
 zsh
 
 p10k configure # 重新配置p10k
 
 ＃ gen ssh key on /.ssh
-ssh-keygen -t ed25519 
+ssh-keygen -t ed25519
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_ed25519
-
 
 sudo apt install git
 git config --global core.editor "vim"
@@ -64,7 +65,6 @@ sudo snap install discord # 安裝dis
 sudo add-apt-repository multiverse
 sudo apt install steam
 
-
 ＃ 安裝注音
 sudo apt install ibus ibus-chewing
 ibus-setup
@@ -72,10 +72,9 @@ ibus-setup
 ＃ 升級ubuntu
 sudo do-release-upgrade -d
 
-sudo apt install nvidia-driver-<版本>   ＃ for
-sudo apt install mesa-vulkan-drivers\   ＃ for amd
+sudo apt install nvidia-driver- for <版本 >＃
+sudo apt install mesa-vulkan-drivers\  ＃ for amd
 
-＃ for proton 
+＃ for proton
 sudo apt install vulkan-tools
 sudo apt upgrade mesa*
-
